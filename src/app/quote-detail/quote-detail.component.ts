ecbb1743f85ab5a25fb0f7d8da35920a80fd4ff7
Frozen Feelings <kiprutohcollo@gmail.com>
2020-04-05 23:48:49 +0300

    edit quote-detail.component.ts

--- src/app/quote-detail/quote-detail.component.ts
+++ src/app/quote-detail/quote-detail.component.ts
@@ -1,19 +1,3 @@
-// import { Component, OnInit } from '@angular/core';
-
-// @Component({
-//   selector: 'app-quote-detail',
-//   templateUrl: './quote-detail.component.html',
-//   styleUrls: ['./quote-detail.component.css']
-// })
-// export class QuoteDetailComponent implements OnInit {
-
-//   constructor() { }
-
-//   ngOnInit(): void {
-//   }
-
-// }
-
 
 import { Component, OnInit, Input, Output, EventEmitter } from "@angular/core";
 import { Quote } from "../quote";
