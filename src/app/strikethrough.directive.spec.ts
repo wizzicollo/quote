1dffb35cc6ad58d2f3efd5fdc276565b980d2b6d
Frozen Feelings <kiprutohcollo@gmail.com>
2020-04-06 00:16:22 +0300

    edit strikethrough.directive.spec.ts

--- src/app/strikethrough.directive.spec.ts
+++ src/app/strikethrough.directive.spec.ts
@@ -1,11 +1,3 @@
-// import { StrikethroughDirective } from './strikethrough.directive';
-
-// describe('StrikethroughDirective', () => {
-//   it('should create an instance', () => {
-//     const directive = new StrikethroughDirective();
-//     expect(directive).toBeTruthy();
-//   });
-// });
 
 import { StrikethroughDirective } from './strikethrough.directive';
 
