ed9b45268156df23675899ea9f017883672a2fa2
Frozen Feelings <kiprutohcollo@gmail.com>
2020-04-06 00:40:36 +0300

    edit app.module.ts

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,39 +1,9 @@
-// import { BrowserModule } from '@angular/platform-browser';
-// import { NgModule } from '@angular/core';
-
-
-// import { AppRoutingModule } from './app-routing.module';
-// import { AppComponent } from './app.component';
-// import { QuoteComponent } from './quote/quote.component';
-// import { QuoteFormComponent } from './quote-form/quote-form.component';
-// import { QuoteDetailComponent } from './quote-detail/quote-detail.component';
-// import { DateCountPipe } from './date-count.pipe';
-// import { StrikethroughDirective } from './strikethrough.directive';
-
-// @NgModule({
-//   declarations: [
-//     AppComponent,
-//     QuoteComponent,
-//     QuoteFormComponent,
-//     QuoteDetailComponent,
-//     DateCountPipe,
-//     StrikethroughDirective
-//   ],
-//   imports: [
-//     BrowserModule,
-//     AppRoutingModule
-//   ],
-//   providers: [],
-//   bootstrap: [AppComponent]
-// })
-// export class AppModule { }
-
 
 import { BrowserModule } from "@angular/platform-browser";
 import { NgModule } from "@angular/core";
 import { FormsModule } from "@angular/forms";
-// import { ParticlesModule } from "angular-particle";
-// import { TimeAgoPipe } from "time-ago-pipe";
+import { ParticlesModule } from "angular-particle";
+import { TimeAgoPipe } from "time-ago-pipe";
 
 import { AppRoutingModule } from "./app-routing.module";
 import { AppComponent } from "./app.component";
@@ -51,13 +21,13 @@ import { QuoteFormComponent } from "./quote-form/quote-form.component";
     StrikethroughDirective,
     DateCountPipe,
     QuoteFormComponent,
-    // TimeAgoPipe
+    TimeAgoPipe
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
     FormsModule,
-    // ParticlesModule
+    ParticlesModule
   ],
   providers: [],
   bootstrap: [AppComponent]
